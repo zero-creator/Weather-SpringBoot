5ef8e4d133858556f2473f28517bee60b29f5a2a
Prashant A <61093851+zero-creator@users.noreply.github.com>
2023-06-22 13:11:57 +0530

    Connection
    Added Cross Origin

--- src/main/java/com/app/controller/UserController.java
+++ src/main/java/com/app/controller/UserController.java
@@ -17,14 +17,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
